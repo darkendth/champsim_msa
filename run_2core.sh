ff96276fc53ed1895870b25d0fda34ea4527132b
darkendth <darkendths@gmail.com>
2022-05-14 18:55:31 +0530

    modified script file

--- run_2core.sh
+++ run_2core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -lt 6 ] || [ "$#" -gt 7 ]; then
+if [ "$#" -lt 7 ] || [ "$#" -gt 8 ]; then
     echo "Illegal number of parameters"
     echo "Usage: ./run_2core.sh [BINARY] [N_WARM] [N_SIM] [N_MIX] [TRACE0] [TRACE1] [OPTION]"
     exit 1
@@ -11,9 +11,10 @@ BINARY=${1}
 N_WARM=${2}
 N_SIM=${3}
 N_MIX=${4}
-TRACE0=${5}
-TRACE1=${6}
-OPTION=${7}
+R_MIX=${5} # just to rename the output file.
+TRACE0=${6}
+TRACE1=${7}
+OPTION=${8}
 
 # Sanity check
 if [ -z $TRACE_DIR ] || [ ! -d "$TRACE_DIR" ] ; then
@@ -49,4 +50,4 @@ if [ ! -f "$TRACE_DIR/$TRACE1" ] ; then
 fi
 
 mkdir -p results_2core_${N_SIM}M
-(./bin/${BINARY} -warmup_instructions ${N_WARM}000000 -simulation_instructions ${N_SIM}000000 ${OPTION} -traces ${TRACE_DIR}/${TRACE0} ${TRACE_DIR}/${TRACE1}) &> results_2core_${N_SIM}M/mix${N_MIX}-${BINARY}${OPTION}.txt
+(./bin/${BINARY} -warmup_instructions ${N_WARM}000000 -simulation_instructions ${N_SIM}000000 ${OPTION} -traces ${TRACE_DIR}/${TRACE0} ${TRACE_DIR}/${TRACE1}) &> results_2core_${N_SIM}M/mix${N_MIX}-${BINARY}${OPTION}-${R_MIX}.txt
--- run_traces_script.sh
+++ run_traces_script.sh
@@ -1,13 +1,13 @@
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 cactusADM_734B.trace.xz gamess_247B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 cactusADM_734B.trace.xz mcf_158B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 cactusADM_734B.trace.xz GemsFDTD_109B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 gamess_316B.trace.xz gamess_247B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 gamess_316B.trace.xz mcf_158B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 gamess_316B.trace.xz GemsFDTD_109B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 bzip2_281B.trace.xz gamess_247B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 bzip2_281B.trace.xz mcf_158B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 bzip2_281B.trace.xz GemsFDTD_109B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 hmmer_546B.trace.xz gamess_247B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 hmmer_546B.trace.xz mcf_158B.trace.xz &
-./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 hmmer_546B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 1 cactusADM_734B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 2 cactusADM_734B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 3 cactusADM_734B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 4 gamess_316B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 5 gamess_316B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 6 gamess_316B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 7 bzip2_281B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 8 bzip2_281B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 9 bzip2_281B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 10 hmmer_546B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 11 hmmer_546B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 12 hmmer_546B.trace.xz GemsFDTD_109B.trace.xz &
 exit
