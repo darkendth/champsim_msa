34d18fc285da723ab6655a30b02bf0264bc5017f
darkendth <darkendths@gmail.com>
2022-05-14 18:19:53 +0530

    add script file to run traces and add result of traces

---
+++ run_traces_script.sh
@@ -0,0 +1,13 @@
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 cactusADM_734B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 cactusADM_734B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 cactusADM_734B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 gamess_316B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 gamess_316B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 gamess_316B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 bzip2_281B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 bzip2_281B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 bzip2_281B.trace.xz GemsFDTD_109B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 hmmer_546B.trace.xz gamess_247B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 hmmer_546B.trace.xz mcf_158B.trace.xz &
+./run_2core.sh bimodal-no-no-no-no-hawkeye_final-2core 50 100 0 hmmer_546B.trace.xz GemsFDTD_109B.trace.xz &
+exit
